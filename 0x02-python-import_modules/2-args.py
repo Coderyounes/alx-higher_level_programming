965eec7f6f3e743736a51cb9966537e75034999c
CoderYounes <younesanas2021@gmail.com>
2023-09-07 16:58:24 +0100

    fix some issue in the number of argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,13 +4,11 @@ if __name__ == "__main__":
     import sys
 
     length = len(sys.argv)
-    if length == 0:
+    if length == 1:
         print("0: arguments.")
-    elif length == 1:
+    elif length == 2:
         print("1 argument:")
-        print("1: {}".format(sys.argv[1]))
     else:
         print("{} arguments:".format(length - 1))
-        for i in range(length):
-            if i != 0:
-                print("{}: {}".format(i, sys.argv[i]))
+    for i in range(1, length):
+        print("{}: {}".format(i, sys.argv[i]))
