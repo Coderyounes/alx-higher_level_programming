f15e1df75838d9958234f6d2cd6945a0f82dea88
CoderYounes <younesanas2021@gmail.com>
2023-09-11 13:43:08 +0100

    make a new tuple to store data to provide multiple return

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
+    new = ()
     if len(sentence) == 0:
-        sentence[0] = None
-    return len(sentence), sentence[0]
+        new = 0, "None"
+    else:
+        new = len(sentence), sentence[0]
+    return new
