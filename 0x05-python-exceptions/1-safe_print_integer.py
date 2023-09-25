a1fb1241d08a8bc9e9afdb042337dc3f9062885e
CoderYounes <younesanas2021@gmail.com>
2023-09-25 22:24:15 +0100

    printing the data direclty without Check

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,8 +2,7 @@
 
 def safe_print_integer(value):
     try:
-        num = int(value)
-        print("{:d}".format(num))
+        print("{:d}".format(value))
         return True
     except ValueError:
         return False
