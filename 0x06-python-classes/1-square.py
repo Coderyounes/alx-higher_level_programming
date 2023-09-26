27c52a513ac6132a6973c95353e036e2998e9fe8
CoderYounes <younesanas2021@gmail.com>
2023-09-26 23:11:41 +0100

    private instance attribute _ size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Class Declaration"""
+
+
+class Square:
+    """Instantiation"""
+    def __init__(self, size):
+        """assigne size to private instance attribute"""
+        self._size = size
