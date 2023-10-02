a1b763c8598a7278b9237f6fc39b5df2a39198cc
CoderYounes <younesanas2021@gmail.com>
2023-10-02 17:11:52 +0100

    3rd Task & Add comments & More tests

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
 
+"""an addition function"""
+
+
 def add_integer(a, b=98):
+    """
+    Return the Total
+
+    Check if the argument are int or float
+    if the Check valid raise a TypeError
+    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
