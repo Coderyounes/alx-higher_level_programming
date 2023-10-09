4903b8698f09d1b0e14831f0871a6ffb9909afe3
CoderYounes <younesanas2021@gmail.com>
2023-10-09 19:39:55 +0100

    separate Class

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,24 +1,10 @@
 #!/usr/bin/python3
 
-"""BaseGeometry is New Class """
+"""Rectangle is an inheretent"""
 
 
-class BaseGeometry:
-    """
-    Class has area() method that Raise an Exception
-    """
-    def area(self):
-        raise Exception("area() is not implemented")
-    """
-    integer_validator function to validate integer
-    Check if the value is int if not raise a TypeError
-    Check if the value is less or equal zero if True raise ValueError
-    """
-    def integer_validator(self, name, value):
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
 
 
 class Rectangle(BaseGeometry):
