e24bb9dfea146f3b7156089d37be0768128d29dd
CoderYounes <younesanas2021@gmail.com>
2023-10-09 17:48:13 +0100

    add more Comments

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,6 +4,11 @@
 
 
 def inherits_from(obj, a_class):
+    """
+    Check if the obj type is inherit of a_class using issubclasse
+    Datatypes are class , print(type(9)) you will be shock by the output
+    Return True  or False
+    """
     if issubclass(type(obj), a_class) and type(obj) is not a_class:
         return True
     return False
