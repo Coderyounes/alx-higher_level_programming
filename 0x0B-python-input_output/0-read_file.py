bdc0a362abf8f14974a9d27ccea65fe05ca10103
CoderYounes <younesanas2021@gmail.com>
2023-10-10 17:43:29 +0100

    missing encoding argument

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,7 +8,8 @@ def read_file(filename=""):
     """
     read_file function , simple function read text from file
     the file is in filename argument
+    encoding argument to handle the text format
     """
-    with open(filename, "r") as f:
+    with open(filename, "r", encoding="UTF-8") as f:
         data = f.read()
     print(data)
