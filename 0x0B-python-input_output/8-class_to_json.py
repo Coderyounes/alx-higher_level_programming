cd389c20be2d9e80ff23557b12ef88d8151d3979
CoderYounes <younesanas2021@gmail.com>
2023-10-11 00:54:06 +0100

    remove the import

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
+
+
 """ Define our function & Import all modules needed"""
-import json
 
 
 def class_to_json(obj):
     """
     Class to json function is function that take instance
     of class and convert into json represetation
-    using json.dumps.
     we take:
         obj: the instace of the classe
         __dict__ : to be selective & include only object that has
         dictionnary representation
     """
-    return json.dumps(obj.__dict__)
+    return obj.__dict__
