eb21b56f86d8d2b84a4f0d26d041f7a48421aea0
CoderYounes <younesanas2021@gmail.com>
2023-10-10 18:17:29 +0100

    write in file & return the number of char written

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+""" function to write in file"""
+
+
+def write_file(filename="", text=""):
+    """
+    This function open the file or create a new one if it not exist
+    using f.write take data from text
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        num = f.write(text)
+    print(num)
