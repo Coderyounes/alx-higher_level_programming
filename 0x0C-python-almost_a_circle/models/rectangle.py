c24675ccc4a85410224931ea59678478fe252924
CoderYounes <younesanas2021@gmail.com>
2023-10-12 22:23:19 +0100

    add checks to my rectangle Class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -12,39 +12,59 @@ class Rectangle(Base):
     """
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     @property
-    def width_get(self):
+    def width(self):
         return self.__width
 
-    @width_get.setter
-    def width_get(self, newwidth):
-        self.__width = newwidth
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
+        else:
+            self.__width = value
 
     @property
-    def height_get(self):
+    def height(self):
         return self.__height
 
-    @height_get.setter
-    def height_get(self, newheight):
-        self.__height = newheight
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value <= 0:
+            raise ValueError("height must be > 0")
+        else:
+            self.__height = value
 
     @property
-    def x_get(self):
+    def x(self):
         return self.__x
 
-    @x_get.setter
-    def x_get(self, new_x):
-        self.__x = new_x
+    @x.setter
+    def x(self, value):
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        elif value < 0:
+            raise ValueError("x must be >= 0")
+        else:
+            self.__x = value
 
     @property
-    def y_get(self):
+    def y(self):
         return self.__y
 
-    @y_get.setter
-    def y_get(self, new_y):
-        self.__y = new_y
+    @y.setter
+    def y(self, value):
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        elif value < 0:
+            raise ValueError("y must be >= 0")
+        else:
+            self.__y = value
