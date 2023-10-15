1b41f50c6fc985e394de5c3055f3eb31fe0da30c
CoderYounes <younesanas2021@gmail.com>
2023-10-15 22:08:31 +0100

    to_dictionnary Task -just a normal output formating

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -109,3 +109,12 @@ class Rectangle(Base):
         for key, value in kwargs.items():
             if hasattr(self, key):
                 setattr(self, key, value)
+
+    def to_dictionary(self):
+        return {
+            'x': self.x,
+            'y': self.y,
+            'id': self.id,
+            'height': self.height,
+            'width': self.width
+        }
