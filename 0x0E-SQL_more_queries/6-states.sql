087165338fac210ca01a2a08a58cb4d54645cf70
CoderYounes <younesanas2021@gmail.com>
2023-11-16 00:52:58 +0100

    forget to add not null after the name

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,3 +1,3 @@
 -- SQL query, create database & table wih columns has auto-generated id
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states (id int NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY, name varchar(256));
+CREATE TABLE IF NOT EXISTS states (id int NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY, name varchar(256) NOT NULL);
