77d8c2c42ec45c76f7e4959d9dee47fa1ed5ba82
CoderYounes <younesanas2021@gmail.com>
2023-11-16 00:54:39 +0100

    ChatGPT to rescue: add USE database_name

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,3 +1,4 @@
 -- SQL query, create database & table wih columns has auto-generated id
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (id int NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY, name varchar(256) NOT NULL);
