805c61191c05cb46fb5eb91be0c6bb77f200a469
CoderYounes <younesanas2021@gmail.com>
2023-11-16 00:50:21 +0100

    unique id with auto increment

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,3 @@
+-- SQL query, create database & table wih columns has auto-generated id
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (id int NOT NULL UNIQUE AUTO_INCREMENT, name varchar(256));
