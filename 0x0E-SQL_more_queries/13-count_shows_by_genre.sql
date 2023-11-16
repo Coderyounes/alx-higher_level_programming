6cfc1c031920edb41d4025151e16e863865ab5b5
CoderYounes <younesanas2021@gmail.com>
2023-11-16 01:43:44 +0100

    fix: the AS

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- SQL query, that count the show
-SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_shows
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
        FROM tv_show_genres
        INNER JOIN tv_genres
        ON tv_genres.id = tv_show_genres.genre_id
