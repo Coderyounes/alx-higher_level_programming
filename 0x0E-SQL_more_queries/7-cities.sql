78254df8ffece380e00b5cdb8288d1e00b37269e
CoderYounes <younesanas2021@gmail.com>
2023-11-16 01:17:11 +0100

    hotfix

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,9 +1,9 @@
 -- SQL Script , creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa)
-CREATE DATABSE IF NOT EXISTS hbtn_0d_usa;
+CREATE database IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities
-	(id int NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
-	state_id int NOT NULL,
-	FOREIGN KEY (state_id) REFERENCES states(id),
-	name varchar(256) NOT NULL
+CREATE table IF NOT EXISTS cities
+       (id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
+       state_id INT NOT NULL,
+       name VARCHAR(256) NOT NULL,
+       FOREIGN KEY (state_id) REFERENCES states(id)
 );
