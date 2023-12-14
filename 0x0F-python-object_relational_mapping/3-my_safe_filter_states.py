17c344ea5b4a24559ee5a2a0ef8c172269c05de8
CoderYounes <younesanas2021@gmail.com>
2023-12-14 21:53:40 +0100

    Make the Code safe from SQLi , but it still not save

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+
+    db_conn = MySQLdb.connect(user=sys.argv[1],
+                              passwd=sys.argv[2],
+                              db=sys.argv[3],
+                              host='localhost',
+                              port=3306)
+
+    state = sys.argv[4]
+    cur = db_conn.cursor()
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    cur.execute(query, (state,))
+
+    results = cur.fetchall()
+
+    for row in results:
+        print(row)
+
+    cur.close()
+    db_conn.close()
