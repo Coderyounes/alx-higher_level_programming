a151a1fa2fb5392b905aae259eac4fc68ad62b82
CoderYounes <younesanas2021@gmail.com>
2023-12-14 22:07:21 +0100

    Task5: JOIN & Aliasing

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+
+    db_conn = MySQLdb.connect(user=sys.argv[1],
+                              passwd=sys.argv[2],
+                              db=sys.argv[3],
+                              host='localhost',
+                              port=3306)
+
+    cur = db_conn.cursor()
+    query = """SELECT c.id, c.name, s.name
+               FROM states AS s, cities AS c
+               WHERE c.state_id = s.id
+               ORDER BY c.id ASC"""
+    cur.execute(query)
+
+    results = cur.fetchall()
+
+    for row in results:
+        print(row)
+
+    cur.close()
+    db_conn.close()
