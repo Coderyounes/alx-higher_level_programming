6063fdddb2d830122a318657f43d4e92a0d3665e
CoderYounes <younesanas2021@gmail.com>
2024-01-06 00:17:44 +0100

    work with urllib for the first time

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+""" """
+import urllib.request
+
+
+with urllib.request.urlopen('https://intranet.alxswe.com/status') as response:
+    html = response.read()
+
+print("Body response")
+print("\t- type: {}".format(type(html)))
+print("\t- content: {}".format(html))
+print("\t- utf8 content: {}".format(html.decode('utf-8')))
