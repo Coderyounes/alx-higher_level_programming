a424937ff8e42e86411108924fe15fa304f6d1c8
CoderYounes <younesanas2021@gmail.com>
2024-01-05 17:35:36 +0100

    refactor the code & use the Binary Search algho

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -15,10 +15,14 @@ def find_peak(list_of_integers):
     """
     if not list_of_integers:
         return None
-    else:
-        peaKnum = list_of_integers[0]
-        length = len(list_of_integers)
-        for i in range(length - 1):
-            if list_of_integers[i] > list_of_integers[i + 1]:
-                peaKnum = list_of_integers[i]
-    return peaKnum
+    low = 0
+    high = len(list_of_integers) - 1
+
+    while low < high:
+        mid = (low + high) // 2
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
+            low = mid + 1
+        else:
+            high = mid
+
+    return list_of_integers[low]
