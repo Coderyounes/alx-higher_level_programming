cecec388cd64f15e7785e33172151d33227a85cf
CoderYounes <younesanas2021@gmail.com>
2024-01-05 15:31:42 +0100

    use other way

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 # Bash Script display the reponse size in byte
-
-if [ -z "$1" ]; then
-    echo "USAGE: $0 <URL>"
-    exit 1
-fi
-
-url="$1"
-
-reponse=$(curl -s -w "%{size_download}" "$url" -o /dev/null)
-echo "$reponse"
+curl -sI "$1" | grep 'Content-Length' | sed 's/^Content-Length: //'
