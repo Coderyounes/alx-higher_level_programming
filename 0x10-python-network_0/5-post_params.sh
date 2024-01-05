2105e3419b8054255748e8c14be60a0876004ca3
CoderYounes <younesanas2021@gmail.com>
2024-01-05 16:30:59 +0100

    add .com after the email

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash Script Perform a POST request with a parameters
-curl -s -X POST "$1" -d 'email=test%40gmail&subject=I%20will%20always%20be%20here%20for%20PLD'
+curl -s -X POST "$1" -d 'email=test%40gmail.com&subject=I%20will%20always%20be%20here%20for%20PLD'
