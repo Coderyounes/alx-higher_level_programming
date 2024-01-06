9bbca59dd2e77875b1b4b5276f0e1ff51624d5c8
CoderYounes <younesanas2021@gmail.com>
2024-01-06 18:27:44 +0100

    remove the dir()

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -9,7 +9,6 @@ if __name__ == '__main__':
     import requests
 
     response = requests.get('https://alx-intranet.hbtn.io/status')
-    print(dir(response))
     print('Body response:')
     print('\t- type: {}'.format(type(response.text)))
     print('\t- content: {}'.format(response.text))
