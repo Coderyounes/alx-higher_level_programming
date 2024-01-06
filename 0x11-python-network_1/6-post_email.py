615b3d88f3fa9b7f8b7f334c3741a74965a5de20
CoderYounes <younesanas2021@gmail.com>
2024-01-06 19:05:17 +0100

    POST using request module

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+    Post request
+"""
+
+if __name__ == '__main__':
+    import requests
+    import sys
+
+    url = sys.argv[1]
+    email = {'email': sys.argv[2]}
+    r = requests.post(url, data=email)
+    print("Your email is : {}".format(sys.argv[2]))
