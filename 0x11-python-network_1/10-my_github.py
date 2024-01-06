3039a1507a1101587139999973d823dc8094be79
CoderYounes <younesanas2021@gmail.com>
2024-01-06 20:10:05 +0100

    Login using the username & Token

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+    Python Script Take you Credentials Dislay ID
+"""
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    u = sys.argv[1]
+    password = sys.argv[2]
+    header = {
+        'Authorization': f'Bearer {password}'
+        }
+
+    r = requests.get('https://api.github.com/{}'.format(u), headers=header)
+    if r.status_code == 200:
+        r_json = r.json()
+        print(r_json['id'])
+    else:
+        print('None')
