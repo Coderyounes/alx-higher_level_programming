ae1cd9077cb1ada01a65b6d4145668503abf5f30
CoderYounes <younesanas2021@gmail.com>
2024-01-06 18:16:25 +0100

    handling HTTPCODE with urllib & try/expect

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+    Handling different Status Code with Try/expect
+        if 200 it return utf-8 decoded response
+        if Error appear expect handle it & return the code
+"""
+
+
+if __name__ == '__main__':
+    import urllib.request
+    import urllib.error
+    import sys
+
+    url = sys.argv[1]
+    try:
+        with urllib.request.urlopen(url) as r:
+            html = r.read()
+            print(html.decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
