e34be1346f27949730714d30f9c9dcb4ea5376dc
CoderYounes <younesanas2021@gmail.com>
2024-01-06 20:15:06 +0100

    fix pycode issue

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -8,11 +8,11 @@ if __name__ == "__main__":
 
     u = sys.argv[1]
     password = sys.argv[2]
-    header = {
+    h = {
         'Authorization': f'Bearer {password}'
         }
 
-    r = requests.get('https://api.github.com/users/{}'.format(u), headers=header)
+    r = requests.get('https://api.github.com/users/{}'.format(u), headers=h)
     if r.status_code == 200:
         r_json = r.json()
         print(r_json['id'])
