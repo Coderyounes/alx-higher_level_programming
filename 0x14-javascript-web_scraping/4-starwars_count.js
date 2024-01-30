33623777958f490fe39ea6a74f91e59dacb656cf
CoderYounes <younesanas2021@gmail.com>
2024-01-30 23:47:10 +0100

    count if cetain actor is in the films

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const URL = process.argv[2];
+const ID = 18;
+
+request(URL, (err, res, body) => {
+  let count = 0;
+  if (!err) {
+    const jsonData = JSON.parse(body);
+    for (let i = 0; i < jsonData.count; i++) {
+      const content = jsonData.results[i].characters;
+      for (let j = 0; j < content.length; j++) {
+        const char = jsonData.results[i].characters;
+        const parts = char[j].split('/');
+        const elementid = parts[parts.length - 2];
+        const userId = String(ID);
+        if (elementid === userId) {
+          count++;
+        }
+      }
+    }
+    console.log(count);
+  } else {
+    console.error(err);
+  }
+});
